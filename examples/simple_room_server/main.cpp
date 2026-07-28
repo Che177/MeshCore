81702214a20ed69ea5ee8fee6b6adaa13b2163a5
Che177 <github@kitagor.com>
2026-07-27 19:55:26 -0700

    room_server: add TLora v2.1 GPIO contact example

--- examples/simple_room_server/main.cpp
+++ examples/simple_room_server/main.cpp
@@ -21,6 +21,42 @@ void halt() {
   while (1) ;
 }
 
+#ifndef ENABLE_GPIO_CONTACT_INPUT
+#define ENABLE_GPIO_CONTACT_INPUT 0
+#endif
+
+#ifndef ENABLE_GPIO_CONTACT_DEBUG
+#define ENABLE_GPIO_CONTACT_DEBUG 0
+#endif
+
+#ifndef GPIO_CONTACT_ACTIVE_LOW
+#define GPIO_CONTACT_ACTIVE_LOW 1
+#endif
+
+#ifndef GPIO_CONTACT_COOLDOWN_MS
+#define GPIO_CONTACT_COOLDOWN_MS 5000
+#endif
+
+#ifndef GPIO_CONTACT_OPEN_TEXT
+#define GPIO_CONTACT_OPEN_TEXT "GPIO contact opened"
+#endif
+
+#ifndef GPIO_CONTACT_CLOSED_TEXT
+#define GPIO_CONTACT_CLOSED_TEXT "GPIO contact closed"
+#endif
+
+#if defined(ENABLE_GPIO_CONTACT_INPUT) && ENABLE_GPIO_CONTACT_INPUT == 1
+  #ifndef GPIO_CONTACT_PIN
+    #error "GPIO_CONTACT_PIN must be defined when ENABLE_GPIO_CONTACT_INPUT=1"
+  #endif
+static bool gpio_contact_active = false;
+static int gpio_contact_last_raw = HIGH;
+static unsigned long gpio_contact_debounce_until = 0;
+static unsigned long gpio_contact_last_change = 0;
+static bool gpio_contact_initialized = false;
+static const unsigned long GPIO_CONTACT_DEBOUNCE_MS = 50;
+#endif
+
 static char command[MAX_POST_TEXT_LEN+1];
 #ifdef ETHERNET_ENABLED
 static char ethernet_command[MAX_POST_TEXT_LEN+1];
@@ -34,6 +70,24 @@ void setup() {
 
 #ifdef HAS_EXTERNAL_WATCHDOG
   external_watchdog.begin();
+#endif
+
+#if defined(ENABLE_GPIO_CONTACT_INPUT) && ENABLE_GPIO_CONTACT_INPUT == 1
+  pinMode(GPIO_CONTACT_PIN, INPUT_PULLUP);
+  int raw = digitalRead(GPIO_CONTACT_PIN);
+  gpio_contact_last_raw = raw;
+  gpio_contact_active = (raw == (GPIO_CONTACT_ACTIVE_LOW ? LOW : HIGH));
+  gpio_contact_debounce_until = millis() + GPIO_CONTACT_DEBOUNCE_MS;
+  gpio_contact_last_change = millis();
+  gpio_contact_initialized = true;
+
+  #if defined(ENABLE_GPIO_CONTACT_DEBUG) && ENABLE_GPIO_CONTACT_DEBUG == 1
+    Serial.printf("GPIO contact enabled: pin=%d active_low=%d initial_raw=%d initial_state=%s\n",
+                  (int)GPIO_CONTACT_PIN,
+                  (int)GPIO_CONTACT_ACTIVE_LOW,
+                  raw == LOW ? 0 : 1,
+                  gpio_contact_active ? "closed" : "opened");
+  #endif
 #endif
 
 #ifdef DISPLAY_CLASS
@@ -118,7 +172,7 @@ void loop() {
   }
 
   if (len > 0 && command[len - 1] == '\r') {  // received complete line
-    command[len - 1] = 0;  // replace newline with C string null terminator
+    command[len - 1] = 0;  // replace carriage return with C string null terminator
     char reply[160];
     reply[0] = 0;
 #ifdef ETHERNET_ENABLED
@@ -145,6 +199,39 @@ void loop() {
     }
     ethernet_send_reply(reply);
     ethernet_command[0] = 0;
+  }
+#endif
+
+#if defined(ENABLE_GPIO_CONTACT_INPUT) && ENABLE_GPIO_CONTACT_INPUT == 1
+  int gpio_raw = digitalRead(GPIO_CONTACT_PIN);
+  bool gpio_active = (gpio_raw == (GPIO_CONTACT_ACTIVE_LOW ? LOW : HIGH));
+
+  if (gpio_raw != gpio_contact_last_raw) {
+    gpio_contact_last_raw = gpio_raw;
+    gpio_contact_debounce_until = millis() + GPIO_CONTACT_DEBOUNCE_MS;
+
+    #if defined(ENABLE_GPIO_CONTACT_DEBUG) && ENABLE_GPIO_CONTACT_DEBUG == 1
+      Serial.printf("GPIO contact raw changed: pin=%d raw=%d active=%d\n",
+                    (int)GPIO_CONTACT_PIN,
+                    gpio_raw == LOW ? 0 : 1,
+                    gpio_active ? 1 : 0);
+    #endif
+  }
+
+  if (gpio_contact_initialized &&
+      (long)(millis() - gpio_contact_debounce_until) >= 0 &&
+      gpio_active != gpio_contact_active &&
+      millis() - gpio_contact_last_change >= GPIO_CONTACT_COOLDOWN_MS) {
+    gpio_contact_active = gpio_active;
+    gpio_contact_last_change = millis();
+
+    const char* msg = gpio_contact_active ? GPIO_CONTACT_CLOSED_TEXT : GPIO_CONTACT_OPEN_TEXT;
+
+    #if defined(ENABLE_GPIO_CONTACT_DEBUG) && ENABLE_GPIO_CONTACT_DEBUG == 1
+      Serial.println(msg);
+    #endif
+
+    the_mesh.addSystemPost(msg);
   }
 #endif
 
